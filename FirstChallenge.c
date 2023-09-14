3a9c0b0de41310a3bf4411b97d067f09a8688cc1
GhizlaneMeq <meqdarghizlane@gmail.com>
2023-09-14 14:03:51 +0100

    4challenges

---
+++ FirstChallenge.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char nom[25];
+    char prenom[45];
+    int age;
+    char sexe[10];
+    long nbt;
+
+    printf("entrer votre nom:\n ");
+    scanf("%s", &nom);
+
+    printf("entrer votre prenom:\n ");
+    scanf(" %[^\n]", &prenom);
+
+
+    printf("entrer votre age:\n ");
+    scanf("%d", &age);
+
+    printf("entrer votre sexe:\n ");
+    scanf("%s", &sexe);
+
+    printf("entrer votre numero de telephone:\n");
+    scanf("%ld", &nbt);
+
+    printf("votre nom est: %s\n",nom);
+    printf("votre prenom est: %s\n",prenom);
+    printf("votre age est: %d\n",age);
+    printf("votre sexe est: %s\n",sexe);
+    printf("votre numero du telephone est:%ld\n ",nbt);
+
+    return 0;
+}
